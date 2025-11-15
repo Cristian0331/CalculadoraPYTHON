058f16eeef71512a9649f97d535911a490b75698
CristianTafur <tafurdiazcristianfelipe159@gmail.com>
2025-11-14 19:16:16 -0500

    Agrega pantalla de entrada para calculos

--- calculadora.py
+++ calculadora.py
@@ -4,4 +4,7 @@ root = tk.Tk()
 root.title("Calculadora Básica")
 root.geometry("300x400")
 
+pantalla = tk.Entry(root, font=("Arial", 20), justify="right")
+pantalla.pack(fill="x", padx=10, pady=10)
+
 root.mainloop()
